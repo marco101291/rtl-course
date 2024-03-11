ee4487c5a2f4f0b203e4137dd0d1c03b4bd3cc0a
Marco Antonio Lievano Escobar <marco.lievano@digitalonus.com>
2024-03-11 12:49:13 -0600

    first commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom-sixteen',
+    verbose: true,
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+    ],
+    "preset": "ts-jest",
+    "testEnvironment": "node",
+    "transform": {
+        "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
+    },
+    "transformIgnorePatterns": [
+        "node_modules/(?!variables/.*)"
+    ]
 }
